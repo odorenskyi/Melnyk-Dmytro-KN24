0e9a4f6a6629e8de21059f87c99c1a6db9ce2e94
Ast1o <dimam4659@gmail.com>
2025-04-02 16:20:33 +0300

    declared functions in the .h file

--- lab08/prj/ModulesMelnyk.h
+++ lab08/prj/ModulesMelnyk.h
@@ -1,6 +1,16 @@
 #ifndef MODULESMELNYK_H_INCLUDED
 #define MODULESMELNYK_H_INCLUDED
 
+using namespace std;
+#include <string>
+#include <iostream>
+
 double s_calculation (double x, double y, double z);
+
+//lab 9
+void wave_height (double grade, double& wave_height, stiting& Desсription);//9.1
+void count_numbers(int n);//9.2
+void binary_count(int N);//9.3
+void task_9_4();//9.4
 
 #endif // MODULESMELNYK_H_INCLUDED
